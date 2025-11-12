9b72303219607a7e3c9c2ddb16a1a58314fa40b0
Steven Knaack <stevenjknaack@gmail.com>
2025-11-12 17:59:39 -0600

    moved exchange members to own file

--- frontend-mobile/src/services/exchanges.ts
+++ frontend-mobile/src/services/exchanges.ts
@@ -1,18 +1,9 @@
 import ModelApi from '@/services/base';
-import { Exchange, ExchangeMember } from '@/types';
+import { Exchange } from '@/types';
 
-// const ExchangeApi = new ModelApi<Exchange, { member?: string; owner?: string }>(
-//   'exchanges/'
-// );
-
-class ExchangeService extends ModelApi<
+export const ExchangeApi = new ModelApi<
   Exchange,
   { member?: string; owner?: string }
-> {}
+>('exchanges/');
 
-export default ExchangeService;
-
-export const ExchangeMemberApi = new ModelApi<
-  ExchangeMember,
-  { exchange?: string }
->('exchange-members/');
+export default ExchangeApi;
